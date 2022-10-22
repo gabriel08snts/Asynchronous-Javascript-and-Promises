6cf62b5182dac246973c8e6a6f98c9048662fe29
Gabriel Aquino <99772558+gabriel08snts@users.noreply.github.com>
2022-10-22 20:27:44 -0300

    Add files via upload

---
+++ node.js
@@ -0,0 +1,13 @@
+// CONECTANDO API COM HTTPS E CALLBACK;
+
+/* Nesta aula vamos verificar se estamos entendendo como que funciona o callback e o 
+assincronismo no JavaScript. */
+
+const https = require('http');
+const API = 'https://jsonplaceholder.typicode.com/users?_limit=2';
+
+https.get(API, res => {
+	console.log(res.statusCode);
+})
+
+console.log('conectando API');
